5f9ef663dd52abe78f145e17b151ba8625ec11ed
LlucSerramitjana <80621909+LlucSerramitjana@users.noreply.github.com>
2021-09-21 13:36:55 +0200

    Add files via upload

---
+++ Pila/src/StackImpl.java
@@ -0,0 +1,16 @@
+public class StackImpl<E> implements Stack<E>{
+    @Override
+    public void push(E e) {
+
+    }
+
+    @Override
+    public E pop() {
+        return null;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
---
+++ Pila/src/StackImplTest.java
@@ -0,0 +1,7 @@
+public class StackImplTest {
+    public static void main(String[] args) {
+        System.out.println("Test");
+        Stack<Integer> s = new StackImpl<Integer>();
+        s.push(5);
+    }
+}
